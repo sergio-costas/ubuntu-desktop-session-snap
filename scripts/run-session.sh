7fb06ea88abda0e3fcf1211ba32b032584adf53b
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2025-05-19 11:08:23 +0200

    Revert moving the RM
    It needs to be called from the same user to work.

--- scripts/run-session.sh
+++ scripts/run-session.sh
@@ -5,13 +5,6 @@ if command -v xdg-user-dirs-update >/dev/null; then
   xdg-user-dirs-update
 fi
 
-# These must be deleted to ensure that another user can
-# launch a session from GDM. Not doing it (or doing it from
-# outside the snap) will prevent login with a different user
-# than the first one that logged in, until the system is reboot.
-rm -rf /tmp/.X11-unix
-rm -rf /tmp/.ICE-unix
-
 # Ensure socket directories exist and have the right permissions
 mkdir -p /tmp/.X11-unix /tmp/.ICE-unix
 chmod 01777 /tmp/.X11-unix /tmp/.ICE-unix
@@ -32,4 +25,10 @@ if ! grep "^snap$" $HOME/.hidden 2>&1 > /dev/null; then
 fi
 
 #exec $SNAP/usr/bin/gnome-shell --display-server --wayland
-exec $SNAP/usr/bin/gnome-session --builtin --session=ubuntu
+$SNAP/usr/bin/gnome-session --builtin --session=ubuntu
+# These must be deleted to ensure that another user can
+# launch a session from GDM. Not doing it (or doing it from
+# outside the snap) will prevent login with a different user
+# than the first one that logged in, until the system is reboot.
+rm -rf /tmp/.X11-unix
+rm -rf /tmp/.ICE-unix
